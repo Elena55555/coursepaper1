6bf581651c32eb5889fa1faeba29eafe5f6f7c3c
Elena <elenela@yandex.ru>
2023-05-15 18:13:44 +0300

    Добрый день! Проверьте, пожалуйста, курсовую работу

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 //        /1. Создать внутри класса с методом main поле типа Employee[10], которое будет выполнять роль «хранилища» для записей о сотрудниках.
 //            Отделы с 1-5;
 //            * Создадим  массив из сотрудников*/
-
         Employee[] staff = new Employee[10];
         staff[0] = new Employee("Иванов Иван Иванович", 1, 75000);
         staff[1] = new Employee("Горбунков Семен Семенович", 2, 50000);
@@ -17,37 +16,35 @@ public class Main {
         staff[7] = new Employee("Муреев Александр Евгеньевич", 3, 44000);
         staff[8] = new Employee("Николаев Николай Николаевич", 4, 66000);
         staff[9] = new Employee("Федоров Федор Федорович", 5, 58000);
-
-
         /* a ) Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).*/
 
 
         for (Employee e : staff) {
             System.out.println(e);
         }
+
 //       /* b ) Посчитать сумму затрат на зарплаты в месяц.
         double sum = 0;
         for (Employee e : staff) {
             sum += e.getSalary();
         }
         System.out.println("Сумма затрат на зарплаты в месяц составила " + sum + " рублей");
-
 //      /*e  Подсчитать среднее значение зарплат.
         int length = staff.length;
         System.out.println("Сумма затрат на зарплаты в месяц в среднем составила " + sum / staff.length + " рублей");
+//
 //      /*c  Найти сотрудника с минимальной зарплатой.
         double minWeight = 200000;
         double maxWeight = -1;
-
         for (Employee e : staff) {
             if (e.getSalary() < minWeight) {
                 // Если значение элемента массива меньше, чем minWeight
                 minWeight = e.getSalary();
-
                 // Записать значение в переменную
             }
 //           /*d Найти сотрудника с максимальной зарплатой.
-           if (e.getSalary() > maxWeight) {
+
+            if (e.getSalary() > maxWeight) {
                 // Если значение элемента массива больше, чем maxWeight
                 maxWeight = e.getSalary();
                 // Записать  значение в переменную
@@ -55,16 +52,13 @@ public class Main {
                 System.out.println("Сотрудник с максим. зарплатой " + staff[0]);
             }
         }
-        System.out.println("Минимальная зарплата " + minWeight);
-        System.out.println("Максимальная зарплата " + maxWeight);
+
 
 //        / * f Получить Ф. И. О. всех сотрудников (вывести в консоль).
         for (Employee e : staff) {
             e.getName();
             System.out.println("Сотрудник  " + e.getName());
         }
-
-
 
     }
 }
